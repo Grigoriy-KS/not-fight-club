e5db78b662efc3d73509b0f7970425e1aca14296
Grigoriy-KS <KsyonzGS@gmail.com>
2025-08-25 19:47:27 +0300

    feat: change the script to go to the Main page

--- not-fight-club/index.js
+++ not-fight-club/index.js
@@ -4,10 +4,14 @@ let characterName = '';
 
 if (registrationForm) {
   submitButton.addEventListener('click', (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     characterName = registrationForm['character-name']?.value;
+    // console.log(characterName);
     if (characterName) {
       window.localStorage.setItem('characterName', characterName);
+      // console.log(window.location.pathname);
+      window.location.pathname = window.location.pathname.slice(0, window.location.pathname.lastIndexOf("/") + 1) + 'home.html';
+      // console.log(window.location.pathname);
     } else {
       alert('Please enter a valid character name');
     }
